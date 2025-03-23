9b77c8b266c36af3204eeec0149cd7a429f27f80
vixithony <vixithony@gmail.com>
2025-03-23 17:32:38 -0300

    Chore: changed enviroment staging

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
 
-  apiBaseUrl: 'http://localhost:8080/oficina', 
+  apiBaseUrl: 'https://rest-api-oficinagestao.onrender.com', 
 
   //STATUS ORDEM SERVICO
   urlGetStatusOrdemServicoById: "/status/",
