6574f1fb019c3d79a9e54ce8a658f70f80034968
vixithony <vixithony@gmail.com>
2025-04-07 21:12:08 -0300

    Chore: new upddated variable

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
 
-  apiBaseUrl: 'https://apiofc.datasweb.com/', 
+  apiBaseUrl: 'https://apiofc.datasweb.com', 
 
   //STATUS ORDEM SERVICO
   urlGetStatusOrdemServicoById: "/status/",
