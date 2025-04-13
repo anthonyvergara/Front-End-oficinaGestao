1028f4ab99f19e151870cf621c56517a66390d11
vixithony <vixithony@gmail.com>
2025-04-13 14:27:38 -0300

    Chore: enviroment to deploy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
 
-  apiBaseUrl: 'http://localhost:8080/oficina',
+  apiBaseUrl: 'https://apiofc.datasweb.com/oficina',
 
   //STATUS ORDEM SERVICO
   urlGetStatusOrdemServicoById: "/status/",
