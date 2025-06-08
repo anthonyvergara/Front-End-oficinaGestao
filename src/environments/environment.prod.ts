b67138ad3e4430096ad67ce10bc68b0ebe5e114c
vixithony <vixithony@gmail.com>
2025-06-08 20:10:16 -0300

    Docs: apibaseurl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
 
-  apiBaseUrl: 'http://localhost:9090/oficina',
+  apiBaseUrl: 'https://apiofc.datasweb.com/oficina',
 
   //STATUS ORDEM SERVICO
   urlGetStatusOrdemServicoById: "/status/",
